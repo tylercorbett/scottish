5dbace27234967183c71864076abcce86d2afd5b
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-25 20:38:30 -0800

    first two order inputs

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Contact.scss';
+import TextField from '@material-ui/core/TextField';
 
 const Contact = () => {
   return (
     <section className="Contact">
-      
+      <h3>Contact</h3>
+      <div className="name-address">
+        <TextField 
+          autoFocus
+          label="Name"
+        />
+        <TextField 
+          label="Address"
+        />
+      </div>
     </section>
   );
 };
