421a5ce67a33d4e8e7530ea1543333f194d18887
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-25 21:14:23 -0800

    quantity

--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Order.scss';
+import TextField from '@material-ui/core/TextField';
 
 const Order = () => {
   return (
     <section className="Order">
       <h3>Order</h3>
-      <div className="item">
-        <label>
-          Item
-        </label>
-        <select>
-          <option>Kilt</option>
-          <option>Hat</option>
-          <option>Shoes</option>
-        </select>
-      </div>
+      <section className="options">
+        <div className="item">
+          <label>
+            Item
+          </label>
+          <select>
+            <option>Kilt</option>
+            <option>Hat</option>
+            <option>Shoes</option>
+          </select>
+        </div>
+        <TextField 
+          label="Quantity"
+          type="number"
+        />
+      </section>
     </section>
   );
 };
