e811404704fdcf51ae3c70582a5cbe60cd4fcabf
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-25 20:25:39 -0800

    header start

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.scss';
+
+const Contact = () => {
+  return (
+    <section className="Contact">
+      
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <div className="logo"><img alt="SCS Logo" src="https://res.cloudinary.com/dmtn5fbdu/image/upload/v1577334048/scottish/logo.jpg"/></div>
+      <h2>SCS Order Form</h2>
+    </header>
+  );
+};
+
+export default Header;
